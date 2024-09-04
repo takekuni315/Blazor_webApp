4dbbe01c35dd473fbbdebeef7188eb0a51fa1224
takekuni315 <ktakeuchi@newcom07.net>
2024-09-04 14:58:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Blazor_webApp/Component.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blazor_webApp
+{
+    public partial class Component : ComponentBase
+    {
+
+    }
+}
